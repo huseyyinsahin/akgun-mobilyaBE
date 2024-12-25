4728eea0c4678c2535e021c1101b36956657210b
huseyyinsahin <huseysahin19@outlook.com>
2024-12-25 16:15:38 +0300

    user

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,8 +4,6 @@ const router = require("express").Router();
 
 router.use("/auth", require("./auth"));
 
-router.use("/user", require("./user.js"));
-
 router.use("/slider", require("./slider.js"));
 
 router.use("/homecard", require("./homeCard.js"));
--- src/routes/user.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-const router = require("express").Router();
-
-const user = require("../controllers/user");
-
-router.route("/").post(user.create);
-
-module.exports = router;
