3abd4b2be55da68e59dedabd5168e64fede06cef
huseyyinsahin <huseysahin19@outlook.com>
2025-01-16 21:47:12 +0300

    upload

--- src/routes/photoGallery.js
+++ src/routes/photoGallery.js
@@ -4,33 +4,9 @@ const router = require("express").Router();
 
 const photogallery = require("../controllers/photoGallery");
 
-const multer = require("multer");
-
 const { isAdmin } = require("../middlewares/permissions");
 
-const path = require("node:path");
-
-const upload = multer({
-  storage: multer.diskStorage({
-    destination: "./upload",
-    filename: function (req, file, returnCallback) {
-      returnCallback(null, Date.now() + "_" + file.originalname);
-    },
-  }),
-  fileFilter: function (req, file, returnCallback) {
-    const allowedExtensions = [".jpg", ".jpeg", ".png"];
-    const fileExtension = path.extname(file.originalname).toLowerCase();
-
-    if (allowedExtensions.includes(fileExtension)) {
-      returnCallback(null, true);
-    } else {
-      returnCallback(
-        new Error("Sadece .jpg, .jpeg ve .png dosyaları yüklenebilir!"),
-        false
-      );
-    }
-  },
-});
+const upload = require("../middlewares/upload");
 
 router
   .route("/")
